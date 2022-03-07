fef095f4ae433df61027a327d01364d90434204a
AlexNicaise <alexandre.nicaise@etu.univ-cotedazur.fr>
2022-03-07 10:40:03 +0100

    creation clientMS

--- client/src/main/java/com/example/client/ClientApplication.java
+++ client/src/main/java/com/example/client/ClientApplication.java
@@ -2,12 +2,13 @@ package com.example.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.example.client")
 public class ClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
 	}
-
 }
