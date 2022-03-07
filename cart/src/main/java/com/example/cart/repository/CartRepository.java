bf949d75a15274372a3d4a0e2e4324b7387619fb
cguibourg-58 <cguibourg-pro@outlook.fr>
2022-03-07 10:04:33 +0100

    build with success

--- cart/src/main/java/com/example/cart/repository/CartRepository.java
+++ cart/src/main/java/com/example/cart/repository/CartRepository.java
@@ -1,5 +1,6 @@
-package com.example.cart;
+package com.example.cart.repository;
 
+import com.example.cart.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
