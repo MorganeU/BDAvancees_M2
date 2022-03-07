9e590ede557358cefb4b8d653099d64afb697a80
cguibourg-58 <cguibourg-pro@outlook.fr>
2022-03-07 11:58:14 +0100

    add beans on order

---
+++ order/src/main/java/com/example/order/beans/CartBean.java
@@ -0,0 +1,29 @@
+package com.example.order.beans;
+
+import java.util.List;
+
+public class CartBean {
+    private Long id;
+    private List<CartItemBean> products;
+
+    public CartBean(Long id, List<CartItemBean> products) {
+        this.id = id;
+        this.products = products;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<CartItemBean> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<CartItemBean> products) {
+        this.products = products;
+    }
+}
