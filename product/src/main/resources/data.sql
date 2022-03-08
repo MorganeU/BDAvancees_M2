40ba6723d03fe14caa0ae2da7eb9239ba702ae92
AlexNicaise <alexandre.nicaise@etu.univ-cotedazur.fr>
2022-03-08 12:00:38 +0100

    New data + Mise en forme HTML

--- product/src/main/resources/data.sql
+++ product/src/main/resources/data.sql
@@ -1,11 +1,6 @@
 INSERT INTO Product(id, name, description, illustration, price)
-values (3, 'gourde', 'Pour boire de l eau', 'https://picsum.photos/200/300', 10),
-(4, 'gourde', 'Pour boire de l eau', 'https://picsum.photos/200', 10);
---        (1, 'chaussettes', 'sans trou', '', 1000);
---        (1, 'chaussettes', 'sans trou', '', 1000);
---        (1, 'chaussettes', 'sans trou', '', 1000);
---        (1, 'chaussettes', 'sans trou', '', 1000);
---        (1, 'chaussettes', 'sans trou', '', 1000);
---        (1, 'chaussettes', 'sans trou', '', 1000);
---        (1, 'chaussettes', 'sans trou', '', 1000);
---        (1, 'chaussettes', 'sans trou', '', 1000);
+values (0, 'Ordinateur', 'Grande qualité graphique', 'https://picsum.photos/id/0/200', 1000),
+    (1, 'Lot de fraises', 'Origine France', 'https://picsum.photos/id/1080/200', 10),
+    (2, 'Guitare', 'Guitare sèche', 'https://picsum.photos/id/145/200', 200),
+    (3, 'Set pour le thé', 'Avec filtre', 'https://picsum.photos/id/225/200', 50),
+    (4, 'Appareil photo argentique', 'Les pellicules ne sont pas comprises dans le prix', 'https://picsum.photos/id/250/200', 70);
