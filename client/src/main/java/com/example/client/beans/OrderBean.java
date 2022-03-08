faa1a2be234ce5624573cf0d03be4faca6b77874
MorganeU <72866789+MorganeU@users.noreply.github.com>
2022-03-08 10:26:05 +0100

    Order et OrderItem + leurs beans + dependencies

--- client/src/main/java/com/example/client/beans/OrderBean.java
+++ client/src/main/java/com/example/client/beans/OrderBean.java
@@ -1,4 +1,60 @@
 package com.example.client.beans;
 
+import java.util.Date;
+import java.util.List;
+
 public class OrderBean {
+    private Long id;
+    private List<OrderItemBean> products;
+    private int paidTotal;
+    private int numberTotalOfProducts;
+    private Date paidDate;
+
+    public OrderBean(Long id, List<OrderItemBean> products, int paidTotal, int numberTotalOfProducts, Date paidDate) {
+        this.id = id;
+        this.products = products;
+        this.paidTotal = paidTotal;
+        this.numberTotalOfProducts = numberTotalOfProducts;
+        this.paidDate = paidDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<OrderItemBean> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<OrderItemBean> products) {
+        this.products = products;
+    }
+
+    public int getPaidTotal() {
+        return paidTotal;
+    }
+
+    public void setPaidTotal(int paidTotal) {
+        this.paidTotal = paidTotal;
+    }
+
+    public int getNumberTotalOfProducts() {
+        return numberTotalOfProducts;
+    }
+
+    public void setNumberTotalOfProducts(int numberTotalOfProducts) {
+        this.numberTotalOfProducts = numberTotalOfProducts;
+    }
+
+    public Date getPaidDate() {
+        return paidDate;
+    }
+
+    public void setPaidDate(Date paidDate) {
+        this.paidDate = paidDate;
+    }
 }
--- order/src/main/java/com/example/order/OrderItem.java
+++ order/src/main/java/com/example/order/OrderItem.java
@@ -1,4 +1,46 @@
 package com.example.order;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class OrderItem {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Long productId;
+    private Integer quantity;
+
+    public OrderItem(){}
+
+    public OrderItem(Long id, Long productId, Integer quantity) {
+        this.id = id;
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
