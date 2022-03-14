07411e9a824f673047f06020772bc4763cc6f559
AlexNicaise <alexandre.nicaise@etu.univ-cotedazur.fr>
2022-03-14 08:56:15 +0100

    Product details get

--- client/src/main/java/com/example/client/controller/ClientController.java
+++ client/src/main/java/com/example/client/controller/ClientController.java
@@ -3,11 +3,16 @@ package com.example.client.controller;
 import com.example.client.beans.ProductBean;
 import com.example.client.proxy.MsProductProxy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ClientController {
@@ -21,5 +26,16 @@ public class ClientController {
         System.out.println(products);
         model.addAttribute("products", products);
         return "index";
+    }
+
+    @GetMapping("/product-detail/{id}")
+    public String getProductDetails(@PathVariable Long id, Model model)
+    {
+        Optional<ProductBean> productDetail = msProductProxy.get(id);
+        if (!productDetail.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Couldn't get product details");
+        }
+        model.addAttribute("product",productDetail.get());
+        return "product-detail";
     }
 }
